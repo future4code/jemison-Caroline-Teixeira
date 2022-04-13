22934a1f0c4b5dc9e53cdd65967b24284842c6d6
caroline da silva teixeira <caroline.76rs@gmail.com>
2022-04-12 22:34:33 -0300

    variaveis

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,109 @@
+//1. 
+//let a = 10
+//let b = 10
+//console.log(b)
+//resposta: 10
+
+// b = 5
+// console.log(a, b);
+// resposta: 10 5
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+// resposta: 10 10 10
+
+// let tempoDeTrabalho = prompt("Quantas horas você trabalha por dia?")
+// let valorPorDia = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${valorPorDia/ tempoDeTrabalho} por hora`)
+// // resposta: R$ 10 por hora
+
+//1.
+ //let nome;
+ //let idade;
+//console.log (typeof nome, idade);
+//resposta: undefined, porque não foi declarado um valor nas variaveis. 
+
+//let seuNome = prompt ("Qual seu nome?");
+
+//let suaIdade = prompt ("Qual sua idade?");
+//let nome = Caroline;
+//let idade = 28;
+//alert(`Seu nome é{Caroline} sua idade é {28}`);
+
+//console.log (typeof seuNome, suaIdade); 
+//resposta: Strings, minhas variavéis representam textos. 
+
+//2.
+//let seSenteBem = prompt ("Você está bem?");
+//let diaDeHoje = prompt ("Hoje é sexta-feira?");
+//let unhaCurta = prompt ("Você cortou a unha?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
