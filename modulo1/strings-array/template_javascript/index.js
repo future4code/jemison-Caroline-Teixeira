8516ebc499bcc1e40019ddcf010a02f1ab7fc02b
caroline da silva teixeira <caroline.76rs@gmail.com>
2022-04-19 23:15:59 -0300

    strings-arrays

---
+++ modulo1/strings-array/template_javascript/index.js
@@ -0,0 +1,64 @@
+// Exercicio 1
+
+// a) undefined
+
+// b) null
+
+// c) 11
+
+// d) array não definido
+
+// e) array não definido
+
+// f) array não definido
+
+//Exercicio 2
+
+// "SUBI NUM ÔNIBUS EM MIRROCOS 27"
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO
+
+//1. let nomeDoUsuario = prompt('Qual é o seu nome?')
+// let emailDoUsuario = prompt("Qual seu email?")
+// console.log("O email", emailDoUsuario, "foi cadastrado com sucesso.", "Seja bem-vinda(o),", nomeDoUsuario,"!");
+
+// 2.let comidasPreferidas = [ 
+//     "Batata frita",
+//     "Churrasco",
+//     "Frango",
+//     "Salada",
+//     "Chocolate"
+// ]
+
+// a) console.log(comidasPreferidas);
+// b) console.log("Essas são minhas comidas preferidas:", comidasPreferidas);
+
+//c)
+//3.
+
+// let listaDeTarefas = []
+// listaDeTarefas = prompt("Qual a primeira tarefa?")
+// listaDeTarefas = prompt("Qual a segunda tarefa?")
+// listaDeTarefas = prompt("Qual a terceira tarefa?")
+// console.log(listaDeTarefas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
